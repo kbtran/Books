43f3f6040892ca9c8391ffee0f9129246fa67a33
Bruce Tran <bruce.tran@gmail.com>
2020-06-06 21:49:33 -0400

    Pitfall #2 Blocking Async Code

--- Books.Api/Books.Api/Controllers/BooksController.cs
+++ Books.Api/Books.Api/Controllers/BooksController.cs
@@ -23,11 +23,20 @@ namespace Books.Api.Controllers
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
+        //[HttpGet]
+        //[BooksResultFilter]
+        //public async Task<IActionResult> GetBooks()
+        //{
+        //    var bookEntities = await _booksRepository.GetBooksAsync();
+        //    return Ok(bookEntities);
+        //}
+
+        // Pitfall #2 Blocking Async Code
         [HttpGet]
         [BooksResultFilter]
-        public async Task<IActionResult> GetBooks()
+        public IActionResult GetBooks()
         {
-            var bookEntities = await _booksRepository.GetBooksAsync();
+            var bookEntities =  _booksRepository.GetBooksAsync().Result;
             return Ok(bookEntities);
         }
 
